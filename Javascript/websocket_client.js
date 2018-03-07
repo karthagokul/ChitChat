28ecaf9cd8382a41cd6f7d3824debe71a2efe800
Gokul Kartha <kartha.gokul@gmail.com>
2018-03-07 23:51:52 +0100

    Javascript function updates

--- Javascript/websocket_client.js
+++ Javascript/websocket_client.js
@@ -8,26 +8,11 @@ function WebSocketSupport() {
 
         var element = document.getElementById("wrapper");
         element.parentNode.removeChild(element);
-	
+
         return;
     }
-		
+
     output = document.getElementById("chatbox");
-
-    websocket = new WebSocket('ws:localhost:9090');
-
-    websocket.onopen = function(e) {
-        writeToScreen("You have have successfully connected to the server");
-    };
-
-
-    websocket.onmessage = function(e) {
-        onMessage(e)
-    };
-
-    websocket.onerror = function(e) {
-        onError(e)
-    };
 }
 
 function onMessage(e) {
@@ -35,86 +20,75 @@ function onMessage(e) {
     if (obj.command == "chat") {
         writeToScreen('<span style="color: blue;"> ' + '<b></span><span style="color: black;">' + obj.sender + '</b>:' + obj.message + '</span>');
     } else if (obj.command == "online") {
-        writeToScreen('<span style="color: blue;">online status changed</span>');
-		updateBuddies(obj);
+        //writeToScreen('<span style="color: blue;">online status changed</span>');
+        updateBuddies(obj);
 
     } else if (obj.command == "logoff") {
         writeToScreen('<span style="color: blue;">' + obj.message + 'Logged Off! </span>');
         updateBuddies(obj);
-    }else if (obj.command == "logon") {
-        writeToScreen('<span style="color: blue;">' + obj.sender + 'Logged In! </span>');
-    } 
-    else {
-        writeToScreen('<span style="color: blue;"> NOT Implemented </span>');
+    } else if (obj.command == "logon") {
+        //writeToScreen('<span style="color: blue;">' + obj.sender + 'Logged In! </span>');
+    } else {
+        //writeToScreen('<span style="color: blue;"> NOT Implemented </span>');
+        // ToDo : Fix me
         updateBuddies(obj);
     }
 }
 
-function updateBuddies(obj)
-{
-	var listDiv = document.getElementById('list-chat');
-        
-        //Deleting all child nodes
-        while (listDiv.firstChild) {
-			listDiv.removeChild(listDiv.firstChild);
-		}
-		
-		//Now Lets update the buddies
-		var ul=document.createElement('ul');
-		listDiv.appendChild(ul);	
-		var lh = document.createElement('lh');
-			var header = document.createTextNode("Online Buddies"); 	 
-     		lh.appendChild(header);
-			ul.appendChild(lh);	
-		for(var i in obj.buddies)
-		{
-			var li = document.createElement('li');
-			var id = obj.buddies[i];
-			var textnode = document.createTextNode(id); 	 
-     		li.appendChild(textnode);
-			ul.appendChild(li);
-		}
+function updateBuddies(obj) {
+    var listDiv = document.getElementById('list-chat');
+
+    //Deleting all child nodes
+    while (listDiv.firstChild) {
+        listDiv.removeChild(listDiv.firstChild);
+    }
+
+    //Now Lets update the buddies
+    var ul = document.createElement('ul');
+    listDiv.appendChild(ul);
+    var lh = document.createElement('lh');
+    var header = document.createTextNode("Online Buddies");
+    lh.appendChild(header);
+    ul.appendChild(lh);
+    for (var i in obj.buddies) {
+        var li = document.createElement('li');
+        var id = obj.buddies[i];
+        var textnode = document.createTextNode(id);
+        li.appendChild(textnode);
+        ul.appendChild(li);
+    }
 }
 
-function onError(e) {
-    writeToScreen('<span style="color: red;">ERROR:</span> ' + e.data);
+function doLogin(server, username) {
+    websocket = new WebSocket('ws:' + server + ':9090');
+    websocket.onopen = function(e) {
+        writeToScreen("You have have successfully connected to the server");
+        var chatname = document.getElementById('username').value;
+        //var msg = '@<b>' + chatname + '</b>: ' + message;
+        var obj = new Object();
+        obj.command = "logon";
+        obj.sender = chatname;
+        var jsonString = JSON.stringify(obj);
+        websocket.send(jsonString);
+    };
+    websocket.onmessage = function(e) {
+        onMessage(e)
+    };
+    websocket.onerror = function(e) {
+        onError(e)
+    };
 }
-
-function doLogin(message) {
-		
-    var chatname = document.getElementById('chatname').value;
-    document.getElementById('msg').value = "";
-    document.getElementById('msg').focus();
-
-    //var msg = '@<b>' + chatname + '</b>: ' + message;
-    var obj = new Object();
-    obj.command = "logon";
-    obj.sender = chatname;
-    var jsonString = JSON.stringify(obj);
-    websocket.send(jsonString);
-
-    writeToScreen(msg);
-}
-
 
 function doSend(message) {
-    /* var validationMsg = userInputSupplied();
-     if (validationMsg !== '') {
-         alert(validationMsg);
-         return;
-     }*/
-
-    var chatname = document.getElementById('chatname').value;
+    var chatname = document.getElementById('username').value;
     document.getElementById('msg').value = "";
     document.getElementById('msg').focus();
-    //var msg = '@<b>' + chatname + '</b>: ' + message;
     var obj = new Object();
     obj.command = "chat";
     obj.sender = chatname;
     obj.message = message;
     var jsonString = JSON.stringify(obj);
     websocket.send(jsonString);
-    //writeToScreen(message);
 }
 
 function writeToScreen(message) {
@@ -124,16 +98,8 @@ function writeToScreen(message) {
     output.appendChild(pre);
 }
 
-function userInputSupplied() {
-    var chatname = document.getElementById('chatname').value;
-    var msg = document.getElementById('msg').value;
-    if (chatname === '') {
-        return 'Please enter your username';
-    } else if (msg === '') {
-        return 'Please the message to send';
-    } else {
-        return '';
-    }
+function onError(e) {
+    writeToScreen('<span style="color: red;">ERROR:</span> ' + e.data);
 }
 
 function browserSupportsWebSockets() {
